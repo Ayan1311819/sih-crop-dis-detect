7ab621ba33fd8fd463843f7ba092cb3ce1934485
Ayan1311819 <ayanchoudhary040@gmail.com>
2024-09-02 13:58:21 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,10 +5,7 @@ import numpy as np
 import gdown
 import os
 
-file_id = '1-AQGIP_4wIGacaTcHmNg0WdbzvXFwk8W'
-url = f'https://drive.google.com/uc?id={file_id}'
-output = 'my_model.h5'
-gdown.download(url, output, quiet=False, use_cookies=False)
+
 model = tf.keras.models.load_model('my_model.h5')
 
 mclass = ['Corn: Common Rust',
