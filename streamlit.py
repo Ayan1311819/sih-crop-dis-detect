ba66b197558665c235a7b460ba274f97f19488b7
Ayan1311819 <ayanchoudhary040@gmail.com>
2024-09-02 14:12:58 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -36,8 +36,6 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=300)
-    st.write("")
     st.write("Classifying...")
     processed_image = preprocess_image(image)
     prediction = model.predict(processed_image)
